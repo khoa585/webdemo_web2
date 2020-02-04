5ff5d15da5a99515d4821890d688a83ad1dbed81
khoa585 <hoduykhoa2k@gmail.com>
2020-02-04 08:56:33 +0700

    make it better

--- Api/Router/Product_router.js
+++ Api/Router/Product_router.js
@@ -2,6 +2,5 @@ var express = require('express')
 var router = express.Router()
 var controller = require('../Controllers/Product_controller')
 router.get('/Product_api',controller.Products)
-router.get('/',controller.index)
 router.post('/Product_api',controller.create_Pd)
 module.exports = router;
