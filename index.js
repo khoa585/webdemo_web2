93c6f2a3eb28efa8f93dce0729d31786ec8f93d8
khoa585 <hoduykhoa2k@gmail.com>
2020-02-04 08:37:31 +0700

    make it better

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const port = 3000;
 const api = require('./Api/Router/Product_router')
 app.use('/api',api);
-app.listen(port,()=>{console.log(`Example app listening on port ${port}`)})
+app.listen(process.env.PORT || port,()=>{console.log(`Example app listening on port ${port}`)})
