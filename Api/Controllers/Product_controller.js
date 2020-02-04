115fb6cce07db0bdb9bd6ca1c4ac2c15b27360e5
khoa585 <hoduykhoa2k@gmail.com>
2020-02-04 08:55:09 +0700

    make it better

--- Api/Controllers/Product_controller.js
+++ Api/Controllers/Product_controller.js
@@ -1,13 +1,9 @@
 var Product = require('../../Models/Db_Product');
-module.exports.index = async (req,res) => {
+module.exports.Products = async function(req,res) {
 	var Products = await Product.find();
 	res.json(Products);
 }
-module.exports.Products = async (req,res) => {
-	var Products = await Product.find();
-	res.json(Products);
-}
-module.exports.create_Pd = async (req,res) => {
+module.exports.create_Pd = async function(req,res){
 	var Products = await Product.create(req.body);
 	res.json(Products);
 }
