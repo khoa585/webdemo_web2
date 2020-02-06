186c0023541c3d85296485d8c899de513da539fe
khoa585 <hoduykhoa2k@gmail.com>
2020-02-06 23:31:43 +0700

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true })
 const router = express.Router()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true })) 
+var mongoose = require('mongoose');
+try {
+   	mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true },(erro)=>{
+       	if(erro){
+           console.log("Erro Connect To DB");
+           console.log(erro);
+       	}else{
+           console.log("Connected to DB");
+       	}
+   	});
+	} catch (error) {
+        	handleError(error); 
+}
 const port = 3000;
 const api = require('./Api/Router/Product_router')
 app.use('/api',api);
